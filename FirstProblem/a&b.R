2df6c67056a9aba08a3689d1440139e1c24e4c62
MateiGoidan <goidan.mateiconstantin@gmail.com>
2024-01-26 21:01:40 +0200

    Finished points a) and b) from the first exercise and also added documentation to them!

---
+++ FirstProblem/a&b.R
@@ -0,0 +1,51 @@
+# Functia frepcomgen creaza tabelul repartitiei comune a v. a. X si Y, cu n + 1 linii unde n
+# reprezinta numarul probabiliatilor lui X si m + 1 linii unde m reprezinta numarul 
+# probabilitatilor lui Y, fara probabilitati asociative. Pe coloana m + 1 se afla valorile
+# probabilitatilorlui X si pe linia n + 1 se afla valorile probabilitatilor lui Y. m si n pot
+# lua valori mai mici de 10^5.
+# 
+# Functia fcomplrepcom completeaza tabelul generat de frepcomgen cu probabilitatile asociative
+
+frepcomgen <- function(n, m) {
+  xVariables <- table(sample(1:n, 100000, replace = TRUE))
+  yVariables <- table(sample(1:m, 100000, replace = TRUE))
+  
+  X <- 1:n
+  X <- lapply(X, function(i) xVariables[as.character(i)]/100000)
+  X <- unlist(X)
+  
+  Y <- 1:m
+  Y <- lapply(Y, function(i) yVariables[as.character(i)]/100000)
+  Y <- unlist(Y)
+  
+  jointDistribution <- matrix(nrow = n + 1, ncol = m + 1)
+  jointDistribution[1:n, m + 1] <- X
+  jointDistribution[n + 1, 1:m] <- Y
+  
+  return(jointDistribution)
+}
+
+fcomplrepcom <- function(jointDistribution) {
+  numbRows <- nrow(jointDistribution)
+  numbCols <- ncol(jointDistribution)
+  
+  X <- jointDistribution[1:(numbRows - 1) , numbCols]
+  Y <- jointDistribution[numbRows , 1:(numbCols - 1)]
+  
+  associatedProbabilities <- matrix(outer(X, Y, "*"), nrow = numbRows - 1, ncol = numbCols - 1)
+  
+  jointDistribution[1:(numbRows - 1), 1:(numbCols - 1)] <- associatedProbabilities
+  #print(jointDistribution)
+  
+  return(jointDistribution)
+}
+
+n <- 7
+m <- 10
+
+result <- frepcomgen(n, m)
+print(result)
+
+result <- fcomplrepcom(result)
+print(result)
+print(sum(result[1:n, 1:m]))
